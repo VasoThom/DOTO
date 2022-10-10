30f2b44ec47e560c78cf2099d5cf43f9e12c7a55
Vaso <vasothom@hotmail.com>
2022-10-10 19:55:19 +0200

    locateStorage

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function addItem(txt) {
-  let li = document.createElement("li");
-  li.innerHTML = txt;
-  list.insertBefore(li, list.childNodes[0]);
-  const delBtn = document.createElement("button");
-  delBtn.innerHTML = "x";
-  delBtn.classList.add("fas");
-  li.append(delBtn);
-  delBtn.addEventListener("click", () => {
-    li.parentNode.removeChild(li);
-    savedTasks = savedTasks.filter((e) => e !== txt);
-    localStorage.setItem("tasks", JSON.stringify(savedTasks));
-  });
-}
 let input = document.querySelector("#todo");
 let btn = document.querySelector("#btn");
 let list = document.querySelector("#list");
 
+function addItem(txt) {
+  let li = document.createElement("li");
+  li.innerHTML = txt;
+  list.append(li);
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("btnX");
+  delBtn.innerHTML = "x";
+  li.append(delBtn);
+  delBtn.addEventListener("click", () => {
+    li.remove(li);
+    // xrisimopoiw filter na gia na min ksanado autes tis ergasies pouu ekana kai esvisa
+    savedTasks = savedTasks.filter((e) => e !== txt);
+
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+  });
+}
+
 let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-// add UI elements for any saved task
 savedTasks.forEach(addItem);
 
 btn.addEventListener("click", () => {
@@ -27,6 +29,7 @@ btn.addEventListener("click", () => {
   } else {
     savedTasks.push(txt);
     localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    // svinei apo input oti aufgabe eixa pros8esei
     input.value = "";
     addItem(txt);
   }
@@ -36,4 +39,10 @@ list.addEventListener("click", (e) => {
   if (e.target.tagName == "LI") {
     e.target.classList.toggle("checked");
   }
+});
+
+const remTasks = document.querySelector(".removeTasks");
+remTasks.addEventListener("click", () => {
+  list.remove(list);
+  localStorage.clear();
 });
